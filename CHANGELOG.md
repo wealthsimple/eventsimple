d7dc5bf65a1c090badeff40e8f6bbac907ec51bc
Zulfiqar Ali <desheikh@gmail.com>
2022-09-15 18:47:31 -0400

    fix cops and setup dummy app

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## 0.0.3 - 2022-09-15
+### Added
+- Fix Cops and setup dummy app
+
 ## 0.0.2 - 2022-09-15
 ### Added
 - Copy eventable base classes from cash-service
--- spec/dummy/spec/rails_helper.rb
+++ spec/dummy/spec/rails_helper.rb
@@ -1,9 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
-
+require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -22,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -41,6 +39,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # You can uncomment this line to turn off ActiveRecord support entirely.
+  # config.use_active_record = false
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -48,7 +49,7 @@ RSpec.configure do |config|
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
-  #     RSpec.describe UsersController, :type => :controller do
+  #     RSpec.describe UsersController, type: :controller do
   #       # ...
   #     end
   #
@@ -60,34 +61,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  require 'access-control/spec_helpers'
-  config.include AccessControl::SpecHelpers
-
-  config.before do
-    stub_auth_service_access_control!
-  end
-end
-
-def deep_symbolize(obj)
-  case obj
-  when Hash
-    obj.deep_symbolize_keys
-  when Array
-    obj.map { |element| deep_symbolize(element) }
-  else
-    obj
-  end
-end
-
-def response_json
-  result = JSON.parse(response.body)
-  deep_symbolize(result)
-end
-
-def headers; end
-
-def make_request!(path)
-  get path, headers: headers, as: :json
-  response_json
 end
