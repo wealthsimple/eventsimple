1a2be542d89dc31e8908e3c4eddb5b1ac1e8b620
Zulfiqar Ali <desheikh@gmail.com>
2022-09-15 18:47:31 -0400

    fix cops and setup dummy app

--- spec/dummy/config/initializers/railway.rb
+++
@@ -1,9 +0,0 @@
-module Ws
-  module Eventable
-    Datadog::Statsd.configure
-    Datadog::Tracer.configure
-    Rollbar.configure(enabled: true)
-    Pheme.configure
-    Sidekiq.configure
-  end
-end
