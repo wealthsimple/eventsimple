c98171d4734d7279756555d347a0bff7ee34d172
Zulfiqar Ali <desheikh@gmail.com>
2022-09-15 18:47:31 -0400

    fix cops and setup dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'implicit_user' => 'implicit_user#index'
-  get 'custom_user' => 'custom_user#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
