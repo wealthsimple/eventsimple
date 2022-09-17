a49fd12c2dd4d763f62f3d3a60c5f09880cf4735
Zulfiqar Ali <desheikh@gmail.com>
2022-09-17 11:01:23 -0400

    add credit

--- lib/eventable/event_dispatcher.rb
+++ lib/eventable/event_dispatcher.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Credit: https://github.com/kickstarter/event-sourcing-rails-todo-app-demo
+
 # Dispatcher implementation.
 module Eventable
   class EventDispatcher
