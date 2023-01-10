fc137028ead6982eeeabc22577d58a331d2d1e36
Martin Crawford <martin.crawford@wealthsimple.com>
2023-01-10 11:57:03 -0500

    Refinement

--- app/controllers/eventable/entities_controller.rb
+++ app/controllers/eventable/entities_controller.rb
@@ -1,27 +1,27 @@
 module Eventable
   class EntitiesController < ApplicationController
+    include Eventable::EventModelResolver
+
     def show
       @model_name = params[:model_name]
       @canonical_id = params[:id]
-      @event_id = params[:e]
-
-      @entity = EventModelResolver.get_entity(model_name: @model_name, canonical_id: @canonical_id)
+      @event_id = params[:e] || -1
 
       # Event History
-      @entity_event_history = EventModelResolver.get_entity_history(
+      @entity_event_history = get_entity_history!(
         model_name: @model_name,
         canonical_id: @canonical_id,
       )
+
+      @entity = get_entity!(model_name: @model_name, canonical_id: @canonical_id)
+
       @latest_event = @entity_event_history.first
-      @selected_event = @entity_event_history.find { |e| e.id == @event_id.to_i }
-      if @selected_event.nil?
-        @selected_event = @latest_event
-        @entity_delta = {}
-      else
-        @entity.reproject(at: @selected_event.created_at)
-        @entity_delta = @entity.changes
-      end
+      @selected_event = @entity_event_history.find { |e| e.id == @event_id.to_i } || @latest_event
+
       @is_historical = @selected_event.id != @latest_event.id
+      entity_at = @is_historical ? @selected_event.created_at : nil
+
+      @entity_properties = get_entity_properties!(model_name: @model_name, canonical_id: @canonical_id, at: entity_at)
     rescue StandardError => e
       @error_message = e.message
     end
--- app/controllers/eventable/models_controller.rb
+++ app/controllers/eventable/models_controller.rb
@@ -1,7 +1,11 @@
 module Eventable
   class ModelsController < ApplicationController
+    include Eventable::EventModelResolver
+
     def show
       @model_name = params[:name]
+
+      @latest_entities = get_lastest_entities!(model_name: @model_name)
     end
   end
 end
