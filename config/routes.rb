de64d855ab21285a7ebd89b529ebbbe894c617e4
Martin Crawford <bemisguided@users.noreply.github.com>
2023-01-20 13:38:46 -0500

    Events UI (#13)
    #### Why
    Adding a UI to view Eventable models.
    
    #### What changed 
    Initial version of the Eventable UI.
    
    Co-authored-by: Zulfiqar Ali <desheikh@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Eventable::Engine.routes.draw do
   root to: 'home#index'
+
+  resources :models, only: [:show], param: :name do
+    member do
+      post :search
+    end
+
+    resources :entities, only: :show
+  end
 end
