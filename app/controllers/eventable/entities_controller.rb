8bbf287eae4307b6f3b586bff2988c1405501b6c
Zulfiqar Ali <desheikh@gmail.com>
2023-01-20 03:13:10 -0500

    entity now shows changes from previous entity

--- app/controllers/eventable/entities_controller.rb
+++ app/controllers/eventable/entities_controller.rb
@@ -3,22 +3,22 @@ module Eventable
     # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
     def show
       @model_name = params[:model_name]
-      model_class = event_classes.find { |d| d.name == @model_name }
+      @model_class = event_classes.find { |d| d.name == @model_name }
       @canonical_id = params[:id]
       @event_id = params[:e] || -1
       @tab_id = params[:t] == 'event' ? 'event' : 'entity'
 
-      @entity = model_class.find_by!(canonical_id: @canonical_id)
-
+      @entity = @model_class.find_by!(canonical_id: @canonical_id)
       @entity_event_history = @entity.events.reverse
 
-      @latest_event = @entity_event_history.first
-      @selected_event = @entity_event_history.find { |e| e.id == @event_id.to_i } || @latest_event
+      @selected_event = @entity_event_history.find { |e|
+        e.id == @event_id.to_i
+      } || @entity_event_history.first
 
-      @is_historical = @selected_event.id != @latest_event.id
-      entity_at = @is_historical ? @selected_event.created_at : nil
+      previous_index = @entity_event_history.find_index { |e| e.id == @selected_event.id } + 1
+      @previous_event = @entity_event_history[previous_index]
 
-      @entity_properties = entity_properties!(entity: @entity, at: entity_at)
+      @entity_changes = changes
     rescue StandardError => e
       @error_message = e.message
     end
@@ -26,31 +26,32 @@ module Eventable
 
     private
 
-    # rubocop:disable Metrics/AbcSize
-    def entity_properties!(entity:, at: nil)
-      changes = {}
-      result = []
-      if at.present?
-        entity.reproject(at: at)
-        changes = entity.changes
-      end
-
-      entity.attributes.each do |attr_name, attr_value|
-        current_value = attr_value
-        historical_value = attr_value
-        if at.present? && changes.key?(attr_name.to_s)
-          current_value = changes[attr_name.to_s][0]
-          historical_value = changes[attr_name.to_s][1]
-        end
-        result.push({
+    def changes
+      current_attributes.map do |attr_name, *|
+        {
           label: attr_name,
-          current_value: current_value,
-          historical_value: historical_value,
-          is_changed: current_value != historical_value,
-        })
+          current_value: current_attributes[attr_name],
+          historical_value: previous_attributes[attr_name],
+          is_changed: current_attributes[attr_name] != previous_attributes[attr_name],
+        }
       end
-      result
     end
-    # rubocop:enable Metrics/AbcSize
+
+    def current_attributes
+      @current_attributes ||= @entity.reproject(at: @selected_event.created_at).attributes.except(
+        *Entity::DEFAULT_IGNORE_PROPS,
+      )
+    end
+
+    def previous_attributes
+      @previous_attributes ||=
+        if @previous_event
+          @entity.reproject(at: @previous_event.created_at).attributes.except(
+            *Entity::DEFAULT_IGNORE_PROPS,
+          )
+        else
+          @model_class.column_defaults
+        end
+    end
   end
 end
--- lib/eventable/entity.rb
+++ lib/eventable/entity.rb
@@ -1,5 +1,7 @@
 module Eventable
   module Entity
+    DEFAULT_IGNORE_PROPS = %w[id lock_version].freeze
+
     def event_driven_by(event_klass, aggregate_id: :canonical_id)
       has_many :events, class_name: event_klass.name.to_s,
         foreign_key: :aggregate_id,
@@ -26,10 +28,8 @@ module Eventable
       end
 
       def reproject(at: nil)
-        default_ignore_props = %w[id lock_version]
-
         event_history = at ? events.where('created_at <= ?', at).load : events.load
-        ignore_props = default_ignore_props.concat(ignored_for_projection).map(&:to_s)
+        ignore_props = (DEFAULT_IGNORE_PROPS + ignored_for_projection).map(&:to_s)
         assign_attributes(self.class.column_defaults.except(*ignore_props))
 
         event_history.each do |event|
