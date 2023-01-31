4c7bd205423a15a970e8610dd0fa3c9cce6fd971
Zulfiqar Ali <desheikh@gmail.com>
2023-01-31 15:29:39 -0500

    avoid using redirect to prevent nginx proxy issues (#20)
    #### Why <!-- A short description of why this change is required -->
    
    
    
    #### What changed <!-- Summary of changes when modifying hundreds of
    lines -->
    
    
    
    <!--
    Consider adding the following sections:
    
    #### How I tested [ Bullets for test cases covered ]
    #### Next steps [ If your PR is part of a few or a WIP, give context to
    reviewers ]
    #### Screenshot [ An image is worth a thousand words ]
    #### Bug/Ticket tracker [ Unnecessary when prefixing branch with JIRA
    ticket, e.g. SECURITY-123-human-readable-thing ]
    -->

--- app/controllers/eventable/models_controller.rb
+++ app/controllers/eventable/models_controller.rb
@@ -6,10 +6,5 @@ module Eventable
       model_event_class = event_classes.find { |d| d.name == @model_name }.event_class
       @latest_entities = model_event_class.last(20).reverse
     end
-
-    def search
-      entity_id = params.require(:search).permit(:entity_id)[:entity_id]
-      redirect_to helpers.model_entity_path(params[:name], entity_id)
-    end
   end
 end
