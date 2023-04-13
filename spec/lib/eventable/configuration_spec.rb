67a876ed16f4443f5e909b55cae272c7bd996666
Masha <masha.ku@gmail.com>
2023-04-13 09:43:43 -0400

    add configuration class settings to make it compatible with modular monolith (#24)
    #### Why <!-- A short description of why this change is required -->
    
    in `athena`, we're using `eventable` from within a `packwerk` component
    with namespaces. This PR adds configuration to help resolve some classes
    
    #### What changed <!-- Summary of changes when modifying hundreds of
    lines -->
    
    * added `dispatcher_class` config
    * added `event_classes` - models to be displayed in the UI
    
    <!--
    Consider adding the following sections:
    
    #### How I tested [ Bullets for test cases covered ]
    #### Next steps [ If your PR is part of a few or a WIP, give context to
    reviewers ]
    #### Screenshot [ An image is worth a thousand words ]
    #### Bug/Ticket tracker [ Unnecessary when prefixing branch with JIRA
    ticket, e.g. SECURITY-123-human-readable-thing ]
    -->
    
    ---------
    
    Co-authored-by: Zulfiqar Ali <desheikh@gmail.com>

--- spec/lib/eventable/configuration_spec.rb
+++ spec/lib/eventable/configuration_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe Eventable::Configuration do
       expect { config.max_concurrency_retries = '1' }.to raise_error(ArgumentError)
     end
   end
+
+  describe '#dispatchers' do
+    it 'defaults to an empty array' do
+      expect(config.dispatchers).to eq([])
+    end
+
+    it 'can be set to an array of strings' do
+      config.dispatchers = ['UserComponent::Dispatcher']
+    end
+
+    it 'raises an error when set to a non-array' do
+      expect { config.dispatchers = 'Eventable::EventDispatcher' }.to raise_error(ArgumentError)
+    end
+  end
 end
