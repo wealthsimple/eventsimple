d35ee14713667a2adc8b39a29a0917daf1a28207
Zulfiqar Ali <desheikh@gmail.com>
2023-04-14 16:23:58 -0400

    add in more tests

--- lib/eventable/engine.rb
+++ lib/eventable/engine.rb
@@ -16,6 +16,22 @@ module Eventable
       unless dispatchers.all? { |dispatcher| dispatcher.superclass == Eventable::Dispatcher }
         raise ArgumentError, 'dispatchers must inherit from Eventable::Dispatcher'
       end
+
+      retry_intervals = Array.new(Eventable.configuration.max_concurrency_retries) { 0 }
+
+      Retriable.configure do |c|
+        c.contexts[:reactor] = {
+          tries: 7,
+          base_interval: 1.0,
+          multiplier: 1.0,
+          rand_factor: 0.0,
+          on: ActiveRecord::RecordNotFound,
+        }
+        c.contexts[:optimistic_locking] = {
+          intervals: retry_intervals,
+          on: ActiveRecord::StaleObjectError,
+        }
+      end
     end
   end
 end
