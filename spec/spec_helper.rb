fa4726625c275afc127844dcf6a07420fd513600
Chris DePaul <cdepaul@gmail.com>
2023-04-15 00:37:53 -0400

    Adding tests (#21)
    We need more tests
    
    lines -->
    
    Only tests added
    
    <!--
    Consider adding the following sections:
    
    reviewers ]
    ticket, e.g. SECURITY-123-human-readable-thing ]
    -->
    
    ---------
    
    Co-authored-by: Zulfiqar Ali <desheikh@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'eventable'
 require 'eventable/support/spec_helpers'
 
 require 'retriable'
+require 'sidekiq/testing'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -25,4 +26,19 @@ RSpec.configure do |config|
   RSpec::Matchers.define_negated_matcher(:not_change, :change)
 
   ActiveRecord::Migration.maintain_test_schema!
+
+  config.after do
+    Sidekiq::Worker.clear_all
+  end
+
+  Retriable.configure do |c|
+    c.tries = 1
+    c.rand_factor = 0.0
+    c.base_interval = 0
+
+    c.contexts.each_key do |context|
+      c.contexts[context][:tries] = 1
+      c.contexts[context][:base_interval] = 0
+    end
+  end
 end
