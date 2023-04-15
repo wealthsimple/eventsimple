fbac12e4c5f11b1ea73774db8d6ce25350d77501
Chris DePaul <cdepaul@gmail.com>
2023-04-15 00:37:53 -0400

    Adding tests (#21)
    #### Why <!-- A short description of why this change is required -->
    
    We need more tests
    
    #### What changed <!-- Summary of changes when modifying hundreds of
    lines -->
    
    Only tests added
    
    <!--
    Consider adding the following sections:
    
    #### How I tested [ Bullets for test cases covered ]
    #### Next steps [ If your PR is part of a few or a WIP, give context to
    reviewers ]
    #### Screenshot [ An image is worth a thousand words ]
    #### Bug/Ticket tracker [ Unnecessary when prefixing branch with JIRA
    ticket, e.g. SECURITY-123-human-readable-thing ]
    -->
    
    ---------
    
    Co-authored-by: Zulfiqar Ali <desheikh@gmail.com>

--- spec/dummy/app/components/user_component/dispatcher.rb
+++ spec/dummy/app/components/user_component/dispatcher.rb
@@ -3,5 +3,7 @@ module UserComponent
     on Events::Created, sync: Reactors::Created::SyncReactor
     on Events::RescuedInvalidTransition, sync: Reactors::Created::SyncReactor
     on Events::RescuedInvalidTransitionWithReraise, sync: Reactors::Created::SyncReactor
+
+    on Events::Created, async: Reactors::Created::AsyncReactor
   end
 end
