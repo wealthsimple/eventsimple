54886582dbf474540104a5dc69920528ddcaad1c
Masha K <mkuznetsova@wealthsimple.com>
2023-04-27 14:59:34 -0400

    ability to provide metadata class

--- lib/eventsimple/configuration.rb
+++ lib/eventsimple/configuration.rb
@@ -3,12 +3,17 @@
 module Eventsimple
   class Configuration
     attr_reader :max_concurrency_retries, :dispatchers
+    attr_writer :metadata_klass
+
     attr_accessor :ui_visible_models
 
     def initialize
       @max_concurrency_retries = 2
       @dispatchers = []
-      @ui_visible_models = [] # internal use only
+
+      # internal use only
+      @ui_visible_models = []
+      @metadata_klass = 'Eventsimple::Metadata'
     end
 
     def max_concurrency_retries=(value)
@@ -23,6 +28,10 @@ module Eventsimple
       raise ArgumentError, 'dispatchers must be an array' unless value.is_a?(Array)
 
       @dispatchers = value
+    end
+
+    def metadata_klass
+      @metadata_klass.constantize
     end
   end
 end
