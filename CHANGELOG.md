618c7d9650428cd5f27d64c246a903fa0607c56c
Zulfiqar Ali <desheikh@gmail.com>
2023-06-19 15:43:08 -0400

    active_job_parent_klass config is removed (#37)
    Fixing an issue where the parent app is unable to find the reactor
    class.
    We've removed the active_job_parent_klass config option and can
    reintroduce it in the future when the use case arises.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 1.1.1 - 2023-06-19
+### Changed
+- Remove active_job_parent_klass config introduced in 1.1.0. There isn't a usecase for it yet,
+  and it was causing loading issues.
+
 ## 1.1.0 - 2023-06-11
 ### Changed
 - Reactors now use ActiveJob instead of Sidekiq directly. This allows for more
--- lib/eventsimple/configuration.rb
+++ lib/eventsimple/configuration.rb
@@ -4,7 +4,6 @@ module Eventsimple
   class Configuration
     attr_reader :max_concurrency_retries
     attr_writer :metadata_klass
-    attr_writer :active_job_parent_klass
     attr_accessor :retry_reactor_on_record_not_found
 
     attr_accessor :ui_visible_models
@@ -13,7 +12,6 @@ module Eventsimple
       @dispatchers = []
       @max_concurrency_retries = 2
       @metadata_klass = 'Eventsimple::Metadata'
-      @active_job_parent_klass = 'ActiveJob::Base'
       @retry_reactor_on_record_not_found = false
 
       @ui_visible_models = [] # internal use only
@@ -34,17 +32,12 @@ module Eventsimple
     end
 
     # rubocop:disable Naming/MemoizedInstanceVariableName
-
     def dispatchers
       @dispatchers_klass_consts ||= @dispatchers.map(&:constantize)
     end
 
     def metadata_klass
       @metadata_klass_const ||= @metadata_klass.constantize
-    end
-
-    def active_job_parent_klass
-      @active_job_parent_klass_const ||= @active_job_parent_klass.constantize
     end
     # rubocop:enable Naming/MemoizedInstanceVariableName
   end
