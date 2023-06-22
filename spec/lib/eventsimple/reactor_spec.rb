baca3f6e0a9bff8254dc8d5f16f9b99cdd9f6c56
Zulfiqar Ali <desheikh@gmail.com>
2023-06-22 10:57:25 -0400

    active job retries on global_id deserialization error (#39)
    When using adapters like Sidekiq, it is possible for the enqueued
    reactors to be triggered _before_ the event is committed.
    
    The previous `around_block` implementation to retry these internally did
    not work as expected as the deserialization actually happens _before_
    the block is triggered.
    
    * Change the behaviour to attempt retries at the point of
    deserialization.

---
+++ spec/lib/eventsimple/reactor_spec.rb
@@ -0,0 +1,55 @@
+RSpec.describe Eventsimple::Reactor do
+  include ActiveJob::TestHelper
+  include RSpec::Rails::TaggedLoggingAdapter
+
+  it 'has a queue name of :eventsimple', type: :job do
+    expect(described_class.queue_name).to eq('eventsimple')
+  end
+
+  context 'with uncommited event' do
+    let!(:event) do
+      UserComponent::Events::Created.new(
+        id: '100000',
+        user: User.new,
+        data: {
+          canonical_id: SecureRandom.uuid,
+          username: 'test',
+          email: 'test@example.com',
+        },
+      )
+    end
+
+    it 'discards the job after retries' do
+      expect_any_instance_of(UserComponent::Reactors::Created::AsyncReactor).not_to receive(:call)
+
+      perform_enqueued_jobs {
+        UserComponent::Reactors::Created::AsyncReactor.perform_later(
+          event,
+        )
+      }
+    end
+  end
+
+  context 'with commited event' do
+    let!(:event) do
+      UserComponent::Events::Created.create(
+        user: User.new,
+        data: {
+          canonical_id: SecureRandom.uuid,
+          username: 'test',
+          email: 'test@example.com',
+        },
+      )
+    end
+
+    it 'runs the reactor' do
+      expect_any_instance_of(UserComponent::Reactors::Created::AsyncReactor).to receive(:call)
+
+      perform_enqueued_jobs {
+        UserComponent::Reactors::Created::AsyncReactor.perform_later(
+          event,
+        )
+      }
+    end
+  end
+end
