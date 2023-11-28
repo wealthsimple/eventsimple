4f956ea1b022f8d543c4edb284e8d71be014b6eb
Lukas Hlavacka <lukas.hlavacka@wealthsimple.com>
2023-11-28 13:28:57 -0500

    Adding support for filtering events by metadata

--- app/controllers/eventsimple/models_controller.rb
+++ app/controllers/eventsimple/models_controller.rb
@@ -2,8 +2,21 @@ module Eventsimple
   class ModelsController < ApplicationController
     def show
       @model_name = params[:name]
+      @metadata = params.permit(metadata: {})[:metadata]
+
+      @metadata_attributes = Eventsimple.configuration.metadata_klass.attribute_names
 
       model_event_class = event_classes.find { |d| d.name == @model_name }.event_class
+
+      if @metadata&.present?
+        @metadata.each do |key, value|
+          next if value.blank?
+          model_event_class = model_event_class.where("metadata->>? = ?", key, value)
+        end
+      else
+        @metadata = {}
+      end
+
       @latest_entities = model_event_class.last(20).reverse
     end
   end
